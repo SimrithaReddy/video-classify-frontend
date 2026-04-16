92a2356058e8cb4716a0389ee04177da14035222
SimrithaReddy <simritha@vridhee.com>
2026-04-16 22:17:21 +0530

    API_BASE_URL

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5000/api";
-// export const API_BASE_URL = "https://video-classify-backend.onrender.com/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://video-classify-backend.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
